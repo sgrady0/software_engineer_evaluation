5b05d8d0f649b03ea934a4add4ad04880b58172c
Shaun Grady <g@f0c.us>
2019-10-29 15:01:29 -0700

    Prevent core module from being imported more than once

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
 
 @NgModule({
   declarations: [],
@@ -9,4 +7,8 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) { throw new Error('CoreModule is already loaded. Import it in the AppModule only'); }
+  }
+}
