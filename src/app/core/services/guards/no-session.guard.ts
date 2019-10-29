96d19e4b6152c8238e58604756d6c938f69bdbf8
Shaun Grady <g@f0c.us>
2019-10-29 16:25:36 -0700

    Build out route guards for SessionService

--- src/app/core/services/guards/no-session.guard.ts
+++ src/app/core/services/guards/no-session.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+import { SessionService } from '../session/session.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoSessionGuard implements CanActivate {
+  private readonly rootRoute: UrlTree = this.router.parseUrl('/');
+
+  constructor(
+    private router: Router,
+    private session: SessionService
+  ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.session.hasSession$.pipe(
+      take(1),
+      map(hasSession => hasSession ? this.rootRoute : true)
+    );
   }
-  
 }
--- src/app/core/services/guards/session.guard.ts
+++ src/app/core/services/guards/session.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+import { SessionService } from '../session/session.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionGuard implements CanActivate {
+  private readonly loginRoute: UrlTree = this.router.parseUrl('/login');
+
+  constructor(
+    private router: Router,
+    private session: SessionService
+  ) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.session.hasSession$.pipe(
+      take(1),
+      map(hasSession => hasSession ? true : this.loginRoute)
+    );
   }
-  
 }
