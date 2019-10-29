83509eaa09e120d8922445f0bda688b268a964dc
Shaun Grady <g@f0c.us>
2019-10-29 15:58:20 -0700

    Hook up core module, core services

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,18 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SessionService } from './services/session/session.service';
+import { SessionGuard } from './services/guards/session.guard';
+import { NoSessionGuard } from './services/guards/no-session.guard';
+import { SearchService } from './services/search/search.service';
 
 @NgModule({
   declarations: [],
+  providers: [
+    SearchService,
+    SessionService,
+    SessionGuard,
+    NoSessionGuard
+  ],
   imports: [
     CommonModule
   ]
