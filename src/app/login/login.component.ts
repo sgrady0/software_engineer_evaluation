42c2a24b98f2e057965ded08ad2aae50dba935b6
Shaun Grady <g@f0c.us>
2019-10-29 17:18:30 -0700

    Build out LoginComponent, scaffold template

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { SessionResponse } from '../core/services/session/session-response.interface';
+import { SessionService } from '../core/services/session/session.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements AfterViewInit {
+  readonly credentials = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
 
-  constructor() { }
+  @ViewChild('usernameControl', { static: true })
+  usernameControl: ElementRef;
 
-  ngOnInit() {
+  isSubmitting = false;
+  errorMessage = '';
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private fb: FormBuilder,
+    private router: Router,
+    private session: SessionService
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.usernameControl.nativeElement.focus();
   }
 
+  submit(): void {
+    const { credentials } = this;
+
+    if (this.isSubmitting) { return; }
+    if (credentials.invalid) {
+      // Expose validation error styling on the template
+      this.credentials.markAllAsTouched();
+      return;
+    }
+
+    this.errorMessage = '';
+    this.isSubmitting = true;
+    this.cdr.markForCheck();
+
+    this.session.authenticate(credentials.value).subscribe({
+      next: this.handleLoginSuccess.bind(this),
+      error: this.handleLoginError.bind(this)
+    });
+  }
+
+  private handleLoginSuccess(): void {
+    this.router.navigateByUrl('/');
+  }
+
+  private handleLoginError({ message }: SessionResponse): void {
+    this.credentials.reset();
+    this.errorMessage = message; // TODO: Display error message once a user story is written and visual design is created.
+    this.isSubmitting = false;
+    this.cdr.markForCheck();
+
+    // Fails to focus if part of current execution queue
+    setTimeout(() => this.usernameControl.nativeElement.focus());
+  }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './login.component';
-
-
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
-export class LoginModule { }
+export class LoginModule {}
