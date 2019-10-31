a3f8f5abec123a5f87cd7a8ca85f8f4cf4c5e963
Shaun Grady <g@f0c.us>
2019-10-31 08:03:35 -0700

    Clean up SearchModule imports

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 import { ModalModule } from '../shared/modules/modal/modal.module';
-import { SearchComponent } from './search.component';
 import { SearchCountComponent } from './search-count/search-count.component';
 import { SearchResultComponent } from './search-result/search-result.component';
-
-
+import { SearchComponent } from './search.component';
 
 @NgModule({
   declarations: [SearchComponent, SearchCountComponent, SearchResultComponent],
